45850526ec531e912e61adbac03b5e7d7dbc418d
ナカザト　エツヤ <169050184+N-Etsuya@users.noreply.github.com>
2024-06-20 04:52:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JS", "React", "Node"],
+        details: {
+            address: "USA",
+            city: "California"
+        }
+    };
+    return member;
+}
